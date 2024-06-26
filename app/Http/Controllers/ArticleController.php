cdea2b8c0b9f768fce3c66c5e0599138cf889166
Sara D <sara.dantonio@live.it>
2024-06-26 21:55:48 +0200

    filled index and create methods, article create view

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index(){
+
+        $articles = Article::all();
+        return view('articles.index', compact('articles') );
+    }
+
+    public function create(){
+
+        return view('articles.create');
+
+    }
+
+
 }
---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,25 @@
+<x-main>
+
+    <div class="mb-md-5 mt-md-4 pb-5">
+        <form action="" method="">
+
+            <div class="form-group">
+                <label for="title">Titolo Articolo</label>
+                <input type="text" class="form-control" id="title" placeholder="Titolo Articolo" name="title">
+            </div>
+            <div class="form-group">
+                <label for="body">Corpo dell'Articolo</label>
+                <input type="text" class="form-control" id="body" placeholder="Testo dell'Articolo" name="body">
+            </div>
+            <div class="mb-3 my-5">
+                <label for="formFile" class="form-label">Default file input example</label>
+                <input class="form-control" type="file" id="formFile">
+            </div>
+
+
+
+
+        </form>
+    </div>
+
+</x-main>
